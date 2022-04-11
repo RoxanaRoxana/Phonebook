36f818d5ddb908e8c09cae20e59137d90e01f263
RoxanaRoxana <roxana0804@gmail.com>
2022-04-11 22:25:10 +0200

    fix

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,4 +1,3 @@
-
 import React from "react";
 import styles from '../components/login/Login.module.css'
 
@@ -6,6 +5,7 @@ const HomePage = () => {
   return (
     <div className={styles.login_section} >
       <h2 className={styles.title}> Phonebook </h2>
+
     
     </div>
   );
