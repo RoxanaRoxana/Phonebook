d3ad5a503f6018840d352afb8d3a845a6aa7d8d2
RoxanaRoxana <roxana0804@gmail.com>
2022-04-11 20:01:49 +0200

    fix

--- src/pages/ContactsPage.js
+++ src/pages/ContactsPage.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { logoutUser } from 'services/api';
+import { useDispatch } from 'react-redux';
 
 const ContactsPage = () => {
+  const dispatch = useDispatch();
   return (
     <div>
       <h2>Contacts</h2>
       <div>
-        <Button onClick={() => dispatch(logoutUser())} name="Sign Out" />
+        <button type='button' onClick={() => dispatch(logoutUser())} name="Sign Out" />
       </div>
     </div>
   );
